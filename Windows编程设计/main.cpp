dd6c83c348c6d02b7056a92efd5c5473e3cf5f87
Tom Yu <543425864@qq.com>
2018-01-06 23:29:23 +0800

    窗口界面

--- Windows编程设计/main.cpp
+++ Windows编程设计/main.cpp
@@ -3,33 +3,42 @@
 #endif 
 
 #include <windows.h>
+#include "resource.h"
+
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
 {
 	// Register the window class.
-	const wchar_t CLASS_NAME[] = L"Sample Window Class";
+	const wchar_t CLASS_NAME[] = L"定时关机程序";
 
 	WNDCLASS wc = {};
 
 	wc.lpfnWndProc = WindowProc;
 	wc.hInstance = hInstance;
 	wc.lpszClassName = CLASS_NAME;
-
+	wc.lpszMenuName = 0;
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	
 	RegisterClass(&wc);
 
 	// Create the window.
 
 	HWND hwnd = CreateWindowEx(
-		0,                              // Optional window styles.
-		CLASS_NAME,                     // Window class
-		L"Learn to Program Windows",    // Window text
-		WS_OVERLAPPEDWINDOW,            // Window style
+		0,                              // Optional window styles.可选窗口风格
+		CLASS_NAME,                     // Window class Window类
+		L"定时关机",    // Window text
+		WS_OVERLAPPEDWINDOW,            // Window style 窗口风格
 
 										// Size and position
 		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-
+		// x坐标，y坐标，x长度,y长度
 		NULL,       // Parent window    
 		NULL,       // Menu
 		hInstance,  // Instance handle
@@ -42,6 +51,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
 	}
 
 	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
 
 	// Run the message loop.
 
@@ -52,13 +62,16 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
 		DispatchMessage(&msg);
 	}
 
-	return 0;
+	return msg.wParam;
 }
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
+	case WM_CREATE:
+		return 0;
+
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		return 0;
@@ -68,10 +81,23 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		PAINTSTRUCT ps;
 		HDC hdc = BeginPaint(hwnd, &ps);
 
-		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-
+		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 0));
+		RECT rect;
+		GetClientRect(hwnd, &rect);
+		static wchar_t a[] = L"定时关机程序，未完成 窗口已绘制次数：00001";
+		DrawText(hdc,a , -1, &rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+		a[23]++;
+		for (int i = 23; i > 19&&a[19]<='9'; i--) 
+		{
+			if (a[i] > '9')
+			{
+				a[i] -= 10;
+				a[i-1] += 1;
+			}
+		}
 		EndPaint(hwnd, &ps);
 	}
+
 	return 0;
 
 	}
